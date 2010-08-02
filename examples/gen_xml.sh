27b6ad63d01432ff855ae836c304ed47d3ced007
Bo Ferri <zazi@smiy.org>
2010-08-02 20:39:00 +0000

    recommendationonto:  * ADDED: new example: simple music recommendation

--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -3,3 +3,4 @@
 # Generate the RDF/XML from the Turtle code
 
 rapper -i turtle -o rdfxml-abbrev N3/music_rec_-_example.n3 > RDF/music_rec_-_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/music_rec_-_simple_example.n3 > RDF/music_rec_-_simple_example.rdf
